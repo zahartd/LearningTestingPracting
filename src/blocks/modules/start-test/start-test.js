b0e95841a5105cd51bab41b73c6b42894c5b0de0
Zaharakatd22 <zaharakatd22@gmail.com>
2020-02-20 10:33:57 +0400

    Начата сетка каталога тестов

---
+++ src/blocks/modules/start-test/start-test.js
@@ -0,0 +1,13 @@
+modules.define('start-test', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
