62a67f992fb4878df00fc047b24d2f6910821111
Zaharakatd22 <zaharakatd22@gmail.com>
2020-02-20 20:55:03 +0400

    Начата разработка прототипа квиза

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,6 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
+import "%modules%/main-info/main-info";
+import "%modules%/preview-tests/preview-tests";
+import "%modules%/start-test/start-test";
+import "%modules%/quiz-progress/quiz-progress";
